1d7a4a5b018779c5cb7b01d0accdad1e4c4e45ac
akiniwa <shin.akiba8@gmail.com>
2013-02-18 16:31:30 +0900

    add planet

--- kepler/openGLsample.hpp
+++ kepler/openGLsample.hpp
@@ -12,61 +12,48 @@ void FreeFall(void){
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		glEnable(GL_DEPTH_TEST);
   
-		glBegin(GL_POLYGON);
-		glColor3f(1.0,0.0, 0.0);
-		 
-		glTranslatef(0.1, 0.0, 0.0);
-		glutSolidSphere( 0.3,50,50);
+		glColor3f(1.0,0.1, 0.1);
 		glPushMatrix();
+		glTranslatef(0.1, 0.0, 0.0);
+		glutSolidSphere( 0.3,30,30);
 		glPopMatrix();
-
-		glEnd();
-
 
 		/* axis */
 		glColor3f(.2, .3, .5);
 		glBegin( GL_LINES );
 		glVertex3d(-1, 0, 0);
 		glVertex3d(1, 0, 0);
-	
-//		glVertex2d(-1, 0);
-//		glVertex2d(1, 0);
 		glEnd();
 
 		glColor3f( .5, .2, .2 );
 		glBegin( GL_LINE_STRIP );
-		for(j=0 ; j<i ; j++)
+		for(j=0 ; j<i ; j++) {
 			glVertex3f( xi[j], yi[j], 0);
-			//glVertex2f( xi[j], yi[j]);
+		}
 		glEnd();
 
 		glColor3f( 1., 1., 1. );
 		glPointSize(4.);
 		glBegin( GL_POINTS );
-
 		for (j=0;j<i;j+=40) {
 			glVertex3f( xi[j], yi[j], 0);
-			//glVertex2f( xi[j], yi[j]);
 		}
 			glVertex3f( xi[i-1], yi[i-1], 0);
-//			glVertex2f( xi[i-1], yi[i-1]);
-
 		glEnd();
 
 		glBegin( GL_LINE_STRIP );
-		for(j=0 ; j<i ; j++)
-			glVertex3f( xi[j]*2.3, yi[j], 0.2);
-			//glVertex2f( xi[j], yi[j]);
+		for(j=0 ; j<i ; j++) {
+			glVertex3f( xi[j]*1.6, yi[j], 0.0f);
+		}
 		glEnd();
 
-		glColor3f( 1., 1., 1. );
-		glPointSize(4.);
-		glBegin( GL_POINTS );
+		glColor3f(0.3,0.7, 0.8);
+		glPushMatrix();
+		glTranslatef( xi[i-1]*1.6, yi[i-1],0.0f);
+		glutSolidSphere(0.1,30,30);
+		glPopMatrix();
 
-		glVertex3f( xi[i-1]*2.3, yi[i-1], 0.2);
-//			glVertex2f( xi[i-1], yi[i-1]);
-
-		glEnd();
+//		glEnd();
 
 
 		glDisable(GL_DEPTH_TEST);
