0c43d4b0edfebf8c5be7ecca391ee43a57b29ac7
akiniwa <shin.akiba8@gmail.com>
2013-02-18 15:55:15 +0900

    good kepler

--- kepler/openGLsample.hpp
+++ kepler/openGLsample.hpp
@@ -1,6 +1,6 @@
 #include <GLUT/glut.h>
 
-int width = 500, height = 500;
+int width = 1000, height = 1000;
 
 float yi[3000];  /* 軌跡データ格納用 */
 float xi[3000];
@@ -8,18 +8,36 @@ int i=0, j=0;
 FILE *fp3;
 
 void FreeFall(void){
+		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		glEnable(GL_DEPTH_TEST);
+  
+		glBegin(GL_POLYGON);
+		glColor3f(1.0,0.0, 0.0);
+		 
+		glTranslatef(0.1, 0.0, 0.0);
+		glutSolidSphere( 0.3,50,50);
+		glPushMatrix();
+		glPopMatrix();
+
+		glEnd();
+
+
 		/* axis */
 		glColor3f(.2, .3, .5);
 		glBegin( GL_LINES );
-		glVertex2d(-1, 0);
-		glVertex2d(1, 0);
+		glVertex3d(-1, 0, 0);
+		glVertex3d(1, 0, 0);
+	
+//		glVertex2d(-1, 0);
+//		glVertex2d(1, 0);
 		glEnd();
 
-		/* draw orbits */
 		glColor3f( .5, .2, .2 );
 		glBegin( GL_LINE_STRIP );
 		for(j=0 ; j<i ; j++)
-			glVertex2f( xi[j], yi[j]);
+			glVertex3f( xi[j], yi[j], 0);
+			//glVertex2f( xi[j], yi[j]);
 		glEnd();
 
 		glColor3f( 1., 1., 1. );
@@ -27,18 +45,40 @@ void FreeFall(void){
 		glBegin( GL_POINTS );
 
 		for (j=0;j<i;j+=40) {
-			glVertex2f( xi[j], yi[j]);
+			glVertex3f( xi[j], yi[j], 0);
+			//glVertex2f( xi[j], yi[j]);
 		}
-			glVertex2f( xi[i-1], yi[i-1]);
+			glVertex3f( xi[i-1], yi[i-1], 0);
+//			glVertex2f( xi[i-1], yi[i-1]);
 
 		glEnd();
+
+		glBegin( GL_LINE_STRIP );
+		for(j=0 ; j<i ; j++)
+			glVertex3f( xi[j]*2.3, yi[j], 0.2);
+			//glVertex2f( xi[j], yi[j]);
+		glEnd();
+
+		glColor3f( 1., 1., 1. );
+		glPointSize(4.);
+		glBegin( GL_POINTS );
+
+		glVertex3f( xi[i-1]*2.3, yi[i-1], 0.2);
+//			glVertex2f( xi[i-1], yi[i-1]);
+
+		glEnd();
+
+
+		glDisable(GL_DEPTH_TEST);
 }
 
 void display(void){
 		glClearColor( .0, .0, .0, .0);
 		glClear( GL_COLOR_BUFFER_BIT );
+		glRotated(0.3, 0.8, 1.0, 0.6);
 		FreeFall();
 		glutSwapBuffers();
+
 }
 
 void timer(int value) {
@@ -61,7 +101,7 @@ void openGL(){
 		}
 
 		/* init gult */
-		glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
+		glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 		glutInitWindowPosition( 0, 0 );
 		glutInitWindowSize( width, height );
 		glutCreateWindow( "FreeFall" );
@@ -73,7 +113,16 @@ void openGL(){
 		/* 2D-setup */
 		glMatrixMode( GL_PROJECTION );
 		glLoadIdentity();
-		gluOrtho2D( -1.0, 1.0, -1.0, 1.0 );
+		/*  
+		gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+		gluPerspective(30.0, (double)width / (double)height, 1.0, 100.0);
+		glTranslated(0.0, 0.0, -0.6);
+*/
+gluPerspective(30.0, (double)width / (double)height, 1.0, 100.0);
+//		glOrtho(-2.0, 2.0, -2.0, 2.0, -8.0, 8.0);
+		glTranslated(0.0, 0.0, -5.0);
+
+//		gluOrtho2D( -1.7, 1.7, -1.7, 1.7 );
 
 		glutMainLoop();
 }
